d8c5762b413673e3a35c1122c22705cfd6b47c83
Kyoung Chan Park <kyoungchan0516@naver.com>
2022-02-17 18:02:23 +0900

    [김서현]  - config

--- src/main/java/com/bbang/ex/SpringExample01Application.java
+++ src/main/java/com/bbang/ex/SpringExample01Application.java
@@ -12,22 +12,11 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.bbang.ex.*")
+
 public class SpringExample01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExample01Application.class, args);
 	}
 	
-	@Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
-
-        return sessionFactory.getObject();
-    }
-
 }
